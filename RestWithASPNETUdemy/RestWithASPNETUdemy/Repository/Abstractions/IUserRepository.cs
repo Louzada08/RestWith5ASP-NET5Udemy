eb8e8f251ffce18bb40275202a320b50641feeab
louzada08 <valuz.anderson.to@gmail.com>
2025-02-24 21:28:49 -0300

    Adicionando JWT configuração

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/Abstractions/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Repository.Abstractions
+{
+    public interface IUserRepository
+    {
+        User? ValidateCredentials(UserVO user);
+    }
+}
