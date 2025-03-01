31dd74a7f7d4bafa95fa141a8e3ca976ef4141ec
louzada08 <valuz.anderson.to@gmail.com>
2025-03-01 15:43:43 -0300

    Concluido autorização e autenticação

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Model/User.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Model/User.cs
@@ -10,13 +10,13 @@ namespace RestWithASPNETUdemy.Model
         [Column("id")]
         public long Id { get; set; }
         [Column("user_name")]
-        public string UserName { get; set; }
+        public string UserName { get; set; } = string.Empty;
         [Column("full_name")]
-        public string FullName { get; set; }
+        public string FullName { get; set; } = string.Empty;
         [Column("password")]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
         [Column("refresh_token")]
-        public string RefreshToken { get; set; }
+        public string? RefreshToken { get; set; } = string.Empty;
         [Column("refresh_token_expiry_time")]
         public DateTime RefreshTokenExpiryTime { get; set; }
     }
