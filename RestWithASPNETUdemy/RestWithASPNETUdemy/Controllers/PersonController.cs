be8f3772b144d9b4c291dfde9e64865c9a136abb
louzada08 <valuz.anderson.to@gmail.com>
2025-03-08 03:04:18 -0300

    corrigido string de conexao bd

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -8,7 +8,7 @@ namespace RestWithASPNETUdemy.Controllers
 {
     [ApiVersion("1")]
     [ApiController]
-    [Authorize("Bearer")]
+   // [Authorize("Bearer")]
     [Route("api/[controller]/v{version:apiVersion}")]
     public class PersonController : ControllerBase
     {
